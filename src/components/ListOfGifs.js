0d2db61c57b4b0a8ed7bcd8e0273a743bf28b83b
Quevedo Fabricio <fabque@gmail.com>
2020-08-31 17:38:13 -0300

    loading rapido cambio state

--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -4,28 +4,30 @@ import getGifs from "../services/getGifs";
 
 export default function ListOfGifs({params}) {
     const {keyword} = params
-    const [loading, setLoading] = useState(false)
 
-    const [gifs, setGifs] = useState([])
 
-    useEffect(function (){
-        setLoading(true)
-        getGifs({keyword}).then(gifs => {
-            setGifs(gifs)
-            setLoading(false)
-        })
+    const [gifs, setGifs] = useState({loading: false, results: []})
+
+    useEffect(function () {
+        setGifs(actualGifs => ({loading: true, results: actualGifs.results}))
+
+        getGifs({keyword})
+            .then(gifs => {
+                setGifs({loading: false, results: gifs})
+            })
     }, [keyword])
 
-    if (loading) return <i>Cargando @</i>
+        if (gifs.loading) return <i>Cargando @</i>
 
-   return <div>{
-    gifs.map(({id, title, url}) =>
-        <Gif
-    key={id}
-    id={id}
-    title={title}
-    url={url}
-    />
-       )
-   }</div>
+        return <div>{
+            gifs.results.map(({id, title, url}) =>
+                <Gif
+                    key={id}
+                    id={id}
+                    title={title}
+                    url={url}
+                />
+            )
+        }</div>
+
 }
