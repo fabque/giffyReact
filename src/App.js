21bb9faffd8ccb8c1729e8f72550448e0482ece4
Quevedo Fabricio <fabque@gmail.com>
2020-08-31 13:00:56 -0300

    Using state to change keyword

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import getGifs from "./services/getGifs";
-import Gif from "./components/Gif";
+import ListOfGifs from "./components/ListOfGifs";
 
 function App() {
-const [gifs, setGifs] = useState([])
-
-    useEffect(function (){
-        getGifs({keyword: 'programming'}).then(gifs => setGifs(gifs))
-    }, [])
+const [keyword, setKeyword] = useState('panda')
 
   return (
     <div className="App">
         <section className="App-content">
-            {
-                gifs.map(sGif =>
-                    <Gif key={sGif.id} title={sGif.title} url={sGif.url} id={sGif.id} ></Gif>
-                )
-            }
+            <button onClick={() => setKeyword('mapache')}>Cambiar</button>
+            <ListOfGifs keyword={keyword} />
         </section>
 
       </div>
