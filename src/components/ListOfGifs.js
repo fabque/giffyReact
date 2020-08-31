a81eb1ec76b69a173356bf8fddbaad8718a0e3da
Quevedo Fabricio <fabque@gmail.com>
2020-08-31 17:16:34 -0300

    loading rapido

--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -4,11 +4,19 @@ import getGifs from "../services/getGifs";
 
 export default function ListOfGifs({params}) {
     const {keyword} = params
+    const [loading, setLoading] = useState(false)
+
     const [gifs, setGifs] = useState([])
 
     useEffect(function (){
-        getGifs({keyword}).then(gifs => setGifs(gifs))
+        setLoading(true)
+        getGifs({keyword}).then(gifs => {
+            setGifs(gifs)
+            setLoading(false)
+        })
     }, [keyword])
+
+    if (loading) return <i>Cargando @</i>
 
    return <div>{
     gifs.map(({id, title, url}) =>
