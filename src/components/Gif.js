7971fa718dc431ea7ad945acd3ad843cd8e8ccb8
Quevedo Fabricio <fabque@gmail.com>
2020-08-31 16:35:34 -0300

    css gif

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 export default function Gif({title, id, url}){
-    return (<div>
+    return (<a href={`#(id)`} className='Gif'>
         <h4>{title}</h4>
         <small>{id}</small>
         <img src={url} alt={title} />
-    </div>)
+    </a>)
 }
