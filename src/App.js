adc8b1abb2bbeb957533e121c489298868f8d258
Quevedo Fabricio <fabque@gmail.com>
2020-08-31 16:45:19 -0300

    add wouter (yarn add wouter)

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React, {useState} from 'react';
 import './App.css';
 import ListOfGifs from "./components/ListOfGifs";
 
+import {Route} from "wouter";
+
 function App() {
 const [keyword, setKeyword] = useState('panda')
 
   return (
     <div className="App">
         <section className="App-content">
-            <button onClick={() => setKeyword('mapache')}>Cambiar</button>
-            <ListOfGifs keyword={keyword} />
+            <h1>Giffy</h1>
+            <Route component={ListOfGifs}
+                   path="/gif/:keyword"/>
+
+
+
         </section>
 
       </div>
--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -2,20 +2,22 @@ import React, {useEffect, useState} from "react";
 import Gif from "./Gif";
 import getGifs from "../services/getGifs";
 
-export default function ListOfGifs({keyword}) {
-
+export default function ListOfGifs({params}) {
+    const {keyword} = params
     const [gifs, setGifs] = useState([])
 
     useEffect(function (){
         getGifs({keyword}).then(gifs => setGifs(gifs))
     }, [keyword])
 
-   return  gifs.map(({id, title, url}) =>
+   return <div>{
+    gifs.map(({id, title, url}) =>
         <Gif
-            key={id}
-            id={id}
-            title={title}
-            url={url}
-              ></Gif>
-    )
+    key={id}
+    id={id}
+    title={title}
+    url={url}
+    />
+       )
+   }</div>
 }
